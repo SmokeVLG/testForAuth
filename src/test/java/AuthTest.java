e176ad2045d18ffd0568806a3be5d2222d96c4a6
mvdenisov <mvdenisov@phoenixit.ru>
2022-07-02 15:12:06 +0300

    Initial commit

---
+++ src/test/java/AuthTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+public class AuthTest {
+    @Test
+    public void test1() {
+        Assert.assertEquals(true, true);
+    }
+}
